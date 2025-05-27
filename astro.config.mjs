c0f85cce23ca57e098827d75c70fc46f461659ec
Angel O.  Salazar <80134042+Ninthharp8@users.noreply.github.com>
2025-05-27 10:18:23 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Ninthharp8.github.io",
+  base:"/AngelOSalazar.github.io/",
+  output: 'static',
   vite: {
       plugins: [tailwindcss()],
   },
