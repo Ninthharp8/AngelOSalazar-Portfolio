09678551bb69a470d7fc39473ab8302fa2172157
Angel O.  Salazar <80134042+Ninthharp8@users.noreply.github.com>
2025-05-27 10:58:58 -0600

    fix path issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Ninthharp8.github.io",
-  base:"/AngelOSalazar-Portfolio/",
+  base:isDev ? '/' : '/AngelOSalazar-Portfolio/',
   output: 'static',
   vite: {
       plugins: [tailwindcss()],
