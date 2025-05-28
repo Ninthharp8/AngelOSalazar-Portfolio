00b43fc872d7c9622a5e4fd7323144bc98e4cda0
Angel Osalde <angelvg268@gmail.com>
2025-05-28 09:52:25 -0600

    intento de deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Ninthharp8.github.io",
-  base:'/AngelOSalazar-Portfolio/',
+  site: "https://AngelOSalazar.github.io",
   output: 'static',
   vite: {
       plugins: [tailwindcss()],
